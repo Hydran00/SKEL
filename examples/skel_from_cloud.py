27b08c211028d236475c1d712872de2ae4213502
hydran00 <davide.nardi-1@unitn.it>
2025-03-09 22:31:13 +0100

    Enhance SKELModelOptimizer: add gradient masking, improve optimization steps, and implement output file saving

--- examples/skel_from_cloud.py
+++ examples/skel_from_cloud.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 from skel.alignment.losses import compute_scapula_loss
 from skel.skel_model import SKEL
 from chamferdist import ChamferDistance
+import os
 
 # Environment setup
 os.environ["LIBGL_ALWAYS_SOFTWARE"] = "1"
@@ -37,19 +38,30 @@ socket_recv.setsockopt_string(zmq.SUBSCRIBE, "")  # Subscribe to all messages
 socket_send = context.socket(zmq.PUB)
 socket_send.bind(f"tcp://*:{ZMQ_PORT_SEND}")
 
+
 class SKELModelOptimizer:
     def __init__(self, skel_model, device=DEVICE):
         self.skel_model = skel_model
-        self.betas = torch.zeros((1, NUM_BETAS), dtype=torch.float32, requires_grad=True, device=DEVICE)
-        self.poses = torch.zeros((1, self.skel_model.num_q_params), dtype=torch.float32, requires_grad=True, device=DEVICE)
-        self.global_orient = torch.zeros((1, 3), dtype=torch.float32, requires_grad=True, device=DEVICE)
-        self.global_position = torch.zeros((1, 3), dtype=torch.float32, requires_grad=True, device=DEVICE)
+        self.betas = torch.zeros(
+            (1, NUM_BETAS), dtype=torch.float32, requires_grad=True, device=DEVICE
+        )
+        self.poses = torch.zeros(
+            (1, self.skel_model.num_q_params),
+            dtype=torch.float32,
+            requires_grad=True,
+            device=DEVICE,
+        )
+        self.global_position = torch.zeros(
+            (1, 3), dtype=torch.float32, requires_grad=True, device=DEVICE
+        )
         self.chamfer_distance = ChamferDistance().to(DEVICE)
         self.skel_skin_vis_offset = [2.0, 0.0, 0.0]
         self.mesh = o3d.geometry.TriangleMesh()
         self.point_cloud = None
-
-
+        self.global_orient_mask = torch.zeros(
+            (1, self.skel_model.num_q_params), dtype=torch.float32, device=DEVICE
+        )
+        self.global_orient_mask[:, :3] = 1.0
 
     def optimize_model(self, target_pc):
         self.target_pc_tensor = self.point_cloud_to_tensor(target_pc).to(DEVICE)
@@ -57,18 +69,77 @@ class SKELModelOptimizer:
         self.init_visualization(target_pc)
 
         # Optimization steps
-        self.optimize([self.global_position], lr=0.05, loss_type="transl", num_iterations=200, tolerance_change=1e-5)
-        self.optimize([self.poses], lr=0.01, loss_type="rot", num_iterations=200, tolerance_change=1e-4)
-        self.optimize([self.poses], lr=0.02, loss_type="transl", num_iterations=200, tolerance_change=1e-4)
-        self.optimize([self.betas], lr=0.002, loss_type="shape", num_iterations=400, tolerance_change=1e-6)
+        mask = torch.zeros_like(self.poses)
+        mask[:, :3] = 1.0  # Only allow gradients on rotation
+        self.optimize(
+            [self.global_position],
+            lr=0.05,
+            loss_type="transl",
+            num_iterations=100,
+            tolerance_change=1e-5,
+        )
+        self.optimize(
+            [self.poses],
+            lr=0.01,
+            loss_type="rot",
+            num_iterations=100,
+            tolerance_change=1e-4,
+            mask=mask,  # Pass the mask to be applied inside optimize()
+        )
 
+        self.optimize(
+            [self.global_position,self.poses],
+            lr=0.02,
+            loss_type="rot",
+            num_iterations=100,
+            tolerance_change=1e-6,
+            mask=mask,  # Pass the mask to be applied inside optimize()
+        )
+        self.optimize(
+            [self.poses],
+            lr=0.02,
+            loss_type="transl",
+            num_iterations=200,
+            tolerance_change=1e-4,
+        )
+
+        self.optimize(
+            [self.betas],
+            lr=0.02,
+            loss_type="shape",
+            num_iterations=200,
+            tolerance_change=1e-6,
+        )
+
+        # return
+        #     self.betas.detach(),
+        #     self.poses.detach(),
+        #     self.global_position.detach(),
+        # )
         return self.get_skel()
 
     def point_cloud_to_tensor(self, point_cloud):
-        return torch.tensor(np.asarray(point_cloud.points), dtype=torch.float32) if isinstance(point_cloud, o3d.geometry.PointCloud) else torch.tensor(point_cloud, dtype=torch.float32)
+        return (
+            torch.tensor(np.asarray(point_cloud.points), dtype=torch.float32)
+            if isinstance(point_cloud, o3d.geometry.PointCloud)
+            else torch.tensor(point_cloud, dtype=torch.float32)
+        )
 
     def compute_loss(self, loss_type, generated_pc):
-        return self.chamfer_distance(generated_pc, self.target_pc_tensor.unsqueeze(0), reverse=True)
+        if loss_type == "transl":
+            return self.chamfer_distance(
+                generated_pc, self.target_pc_tensor.unsqueeze(0), reverse=True
+            )
+        elif loss_type == "rot":
+            return self.chamfer_distance(
+                generated_pc, self.target_pc_tensor.unsqueeze(0), reverse=True
+            )
+        elif loss_type == "shape":
+            return self.betas.pow(2).sum() + self.chamfer_distance(
+                generated_pc, self.target_pc_tensor.unsqueeze(0), reverse=True
+            )
+        else:
+            raise ValueError(f"Invalid loss type: {loss_type}")
 
     def get_skel(self):
         return self.skel_model.forward(
@@ -79,7 +150,9 @@ class SKELModelOptimizer:
             skelmesh=True,
         )
 
-    def optimize(self, params, lr, loss_type, num_iterations, tolerance_change):
+    def optimize(
+        self, params, lr, loss_type, num_iterations, tolerance_change, mask=None
+    ):
         optimizer = torch.optim.AdamW(params, lr=lr)
         last_loss = 1e6
         pbar = tqdm(range(num_iterations))
@@ -87,25 +160,39 @@ class SKELModelOptimizer:
             optimizer.zero_grad()
             skel_output = self.get_skel()
             skin_vertices = skel_output.skin_verts
+            # skel_vertices = skel_output.skel_verts
             loss1 = self.compute_loss(loss_type, skin_vertices)
             loss2 = 1.0 * compute_scapula_loss(self.poses)
+            skin_vertices_vis = skin_vertices.cpu().detach().clone().numpy()
+            # skel_vertices_vis = skel_vertices.cpu().detach().clone().numpy()
             loss = loss1 + loss2
             loss.backward()
-            optimizer.step()
+            # Apply the mask to prevent updates on non-rotation parameters
+            if mask is not None and self.poses.grad is not None:
+                self.poses.grad *= mask
             loss_change = abs(last_loss - loss.item())
             if loss_change < tolerance_change:
                 print(f"Converged at iteration {i}, Loss: {loss.item():.6f}")
                 break
             last_loss = loss.item()
-            pbar.set_description(f"Iteration {i}, Loss: {loss.item():.2f}, Loss change: {loss_change:.6f}")
-            self.update_visualization(skin_vertices.cpu().detach().numpy())
+            optimizer.step()
+            if i % 1 == 0:  # Update visualization every 10 iterations
+                # print(f"Iteration {i}, Loss: {loss.item():.6f}")
+                pbar.set_description(
+                    f"Iteration {i}, Loss: {loss.item():.2f}, Loss change: {loss_change:.6f}"
+                )
+                self.update_visualization(skin_vertices_vis)
+
+        print(f"Final {loss_type} loss: {last_loss:.6f}")
 
     def init_visualization(self, target_pc):
         # Open3D visualization setup
         self.vis = o3d.visualization.Visualizer()
         self.vis.create_window()
         self.vis.clear_geometries()
-        ref_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.6, origin=[0, 0, 0])
+        ref_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=0.6, origin=[0, 0, 0]
+        )
         self.vis.add_geometry(ref_frame)
         target_pc.paint_uniform_color([0, 0, 1])
         self.vis.add_geometry(target_pc)
@@ -130,8 +217,13 @@ class SKELModelOptimizer:
     def close_visualization(self):
         self.vis.destroy_window()
 
-def send_optimized_results(data):
-    socket_send.send(pickle.dumps(data))
+
+def send_optimized_results(data, output_folder):
+    content = pickle.dumps(data)
+    socket_send.send(content)
+    if output_folder is not None:
+        with open(output_folder + "optimized_skel_params.pkl", "wb") as f:
+            pickle.dump(data, f)
     print("Optimized results sent back.")
 
 
@@ -145,9 +237,36 @@ def receive_point_cloud():
     return point_cloud
 
 
-def main():
+def main(args=None):
+
+    # get arguments
+    parser = argparse.ArgumentParser(description="Skeleton fitting from point cloud")
+    parser.add_argument(
+        "--use_initialization",
+        type=bool,
+        default=False,
+        help="Use the last found parameters as initialization",
+    )
+    parser.add_argument(
+        "--output_folder",
+        type=str,
+        default=os.path.expanduser("~") + "/temp/skel_fitting/",
+        help="Output folder to save optimized parameters",
+    )
+    args = parser.parse_args(args)
+
     skel_model = SKEL("male").to(DEVICE)
     optimizer = SKELModelOptimizer(skel_model)
+    
+    output_folder = args.output_folder
+    
+    if args.use_initialization:
+        with open(output_folder + "optimized_skel_params.pkl", "rb") as f:
+            # params = pickle.load(f)
+            # optimizer.betas = torch.tensor(params["betas"][:,:,0], device=DEVICE)
+            # optimizer.poses = torch.tensor(params["body_pose"][:,:,0], device=DEVICE)
+            # optimizer.global_position = torch.tensor(params["global_pos"][:,0], device=DEVICE)
+            print("Using last found parameters as initialization.")    
 
     while True:
         print("Waiting for point cloud...")
@@ -156,13 +275,15 @@ def main():
         ref_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
             size=0.6, origin=[0, 0, 0]
         )
+
         # def radians_from_deg(rad):
         #     return rad * np.pi / 180
+
         # rot = pc.get_rotation_matrix_from_xyz((-np.pi / 2, 0, 0))
         # pc.rotate(rot, center=(0, 0, 0))
-        # rot = pc.get_rotation_matrix_from_xyz((0, np.pi/6, 0))
+        # rot = pc.get_rotation_matrix_from_xyz((0, np.pi / 6, 0))
         # pc.rotate(rot, center=(0, 0, 0))
-        # rot = pc.get_rotation_matrix_from_xyz((-np.pi/10, 0, 0))
+        # rot = pc.get_rotation_matrix_from_xyz((-np.pi / 10, 0, 0))
         # pc.rotate(rot, center=(0, 0, 0))
         # rot = pc.get_rotation_matrix_from_xyz((0, radians_from_deg(20), 0))
         # pc.rotate(rot, center=(0, 0, 0))
@@ -174,12 +295,10 @@ def main():
         points = np.asarray(pc.points)
         idx = np.where(points[:, 2] > -0.4)
         pc.points = o3d.utility.Vector3dVector(points[idx])
-        pc.colors = o3d.utility.Vector3dVector(np.asarray(pc.colors)[idx]) 
-
+        pc.colors = o3d.utility.Vector3dVector(np.asarray(pc.colors)[idx])
 
         o3d.visualization.draw_geometries([pc, ref_frame])
 
-    
         # downsample
         pc = pc.voxel_down_sample(voxel_size=0.02)
 
@@ -189,20 +308,30 @@ def main():
         # send optimized results back
         skin_vertices = optimized_skel.skin_verts.cpu().detach().numpy()
         skin_faces = optimizer.skel_model.skin_f.cpu().detach().numpy()
+        skin_mesh = o3d.geometry.TriangleMesh()
+        skin_mesh.vertices = o3d.utility.Vector3dVector(skin_vertices[0])
+        skin_mesh.triangles = o3d.utility.Vector3iVector(skin_faces)
         skel_verts = optimized_skel.skel_verts.cpu().detach().numpy()
         skel_faces = optimizer.skel_model.skel_f.cpu().detach().numpy()
+        skel_mesh = o3d.geometry.TriangleMesh()
+        skel_mesh.vertices = o3d.utility.Vector3dVector(skel_verts[0])
+        skel_mesh.triangles = o3d.utility.Vector3iVector(skel_faces)
         results = {
             "skel_verts": skel_verts.tolist(),
             "skel_faces": skel_faces.tolist(),
             "skin_verts": skin_vertices.tolist(),
             "skin_faces": skin_faces.tolist(),
+            "joints_ori": optimized_skel.joints_ori.cpu().detach().numpy().tolist(),
             "betas": optimized_skel.betas.cpu().detach().numpy().tolist(),
             "body_pose": optimized_skel.poses.cpu().detach().numpy().tolist(),
             "global_pos": optimized_skel.trans.cpu().detach().numpy().tolist(),
         }
-        send_optimized_results(results)
+        send_optimized_results(results, output_folder)
+        o3d.io.write_triangle_mesh(output_folder + "optimized_skel.ply", skel_mesh)
+        o3d.io.write_triangle_mesh(output_folder + "optimized_skin.ply", skin_mesh)
         print("Optimization cycle complete.")
         optimizer.close_visualization()
+
 
 if __name__ == "__main__":
     main()
